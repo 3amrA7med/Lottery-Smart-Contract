b4fa79c7fb439c1b1b860c37d8d96507f98a9691
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2020-10-30 23:57:18 +0200

    Added Lottery contract source code, compile code, deploy code and an initial test

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    // Token,
+    'https://rinkeby.infura.io/v3/71ea9b4668504654bbae01672c63f757'
+);
+
+const web3 = new Web3(provider);
+
+const INITIAL_MESSAGE = 'INBOX SMART CONTRACT';
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    let lottery = await new web3.eth.Contract(JSON.parse(interface))
+         .deploy({ data: bytecode})
+         .send({ from: accounts[0], gas: '1000000' });
+         console.log("Contract Deployed To: ", lottery.options.address);
+}
+deploy();
